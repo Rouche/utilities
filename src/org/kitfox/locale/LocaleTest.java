ca77f654fd9caa48798dfd711df8964c66befbd6
Jean-Francois Larouche <rouche@gmail.com>
2019-06-18 15:29:46 -0400

    Locale server test

---
+++ src/org/kitfox/locale/LocaleTest.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package org.kitfox.locale;
+
+import java.util.Locale;
+
+import org.junit.Test;
+
+/**
+ * @author larj15
+ */
+public class LocaleTest {
+
+    @Test
+    public void test() throws Exception {
+        System.out.println(Locale.getDefault());
+    }
+}
