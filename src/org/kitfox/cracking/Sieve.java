e23e769e50324713d667068973e16654d68b2314
Rouche <rouche@gmail.com>
2019-08-15 11:33:41 -0400

    Primes

--- src/org/kitfox/cracking/Sieve.java
+++ src/org/kitfox/cracking/Sieve.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Sieve {
 
     @Test
-    public void testDrawLine() {
+    public void testPrimes() {
         int[] result = generatePrimes(100);
 
         for (int p : result) {
